1cfe0817dc21692ce2f6c5c2ff48a44ba376ecba
Thomas Martyniuck <thomas.martyniuck@gmail.com>
2019-12-20 10:23:29 +0000

    Contact Create Action for Saving to the Database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,11 @@
 ActiveRecord::Schema.define(version: 20191219200626) do
 
   create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
