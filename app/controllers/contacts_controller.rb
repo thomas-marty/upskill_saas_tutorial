3b1e759f4e32fd2013aa763c3a5f268b00b95536
Thomas Martyniuck <thomas.martyniuck@gmail.com>
2019-12-23 15:49:18 +0000

    add the action of delivering the email from contact form - trigger the mailer form th controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,8 +8,12 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     @contact.save
     if @contact.save
+      name=params[:contact][:name]
+      email=params[:contact][:email]
+      body=params[:contact][:comments]
+      ContactMailer.contact_email(name, email, body).deliver
       flash[:success]="Message sent"
-       redirect_to new_contact_path
+      redirect_to new_contact_path
     else
        flash[:danger]=@contact.errors.full_messages.join(", ")
        redirect_to new_contact_path
